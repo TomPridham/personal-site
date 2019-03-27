b2ee020fea2e38c9d3060487f7217fbc57fd6e48
Tom Pridham <pridham.tom@gmail.com>
2019-03-26 21:29:32 -0600

    writing to a file now :tada:

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 #![feature(proc_macro_hygiene)]
 
 extern crate maud;
+use std::error::Error;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
 use maud::html;
 
 fn main() {
+    let path = Path::new("doc/index.html");
+    let display = path.display();
+
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", display, why.description()),
+        Ok(file) => file,
+    };
     let name = "Tom Pridham";
     let markup = html! {
         p { "Hi, " (name) "!" }
     };
-    println!("{}", markup.into_string())
+    match file.write_all(&markup.into_string().as_bytes()) {
+        Err(why) => {
+            panic!("couldn't write to {}: {}", display, why.description())
+        },
+        Ok(_) => println!("success!")
+    }
 }
