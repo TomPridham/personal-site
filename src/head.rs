8f0427da56c73f4151541c9cb0a9abee4a105406
Tom Pridham <pridham.tom@gmail.com>
2019-06-06 18:05:10 -0600

    made the css less bad

--- src/head.rs
+++ src/head.rs
@@ -10,7 +10,7 @@ pub fn head() -> Markup {
             meta name="description" content="portfolio site for tom pridham. he likes being good at things.";
             title { "tompridham.me "}
             link rel="shortcut icon" href="favicon.ico" type="image/x-icon";
-            link rel="stylesheet" href="main.css";
+            link rel="stylesheet" href="app.css";
         }
     }
 }
