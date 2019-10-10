cf61a067dc2020cb3afdbd9e307b10f05f9988d4
Tom Pridham <pridham.tom@gmail.com>
2019-10-09 18:49:00 -0600

    add rust toolchain file, updated resume

--- src/cv.rs
+++ src/cv.rs
@@ -7,7 +7,7 @@ use std::fs::read_to_string;
 use std::path::Path;
 
 #[derive(Debug, Serialize, Deserialize)]
-struct CVItem {
+struct Job{
     company: String,
     duties: Vec<String>,
     notable_achievements: Vec<String>,
@@ -16,32 +16,94 @@ struct CVItem {
     title: String,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Project{
+    description: String,
+    technologies: Vec<String>,
+    title:String
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct CV{
+    projects: Vec<Project>,
+    work: Vec<Job>
+}
+
 pub fn cv() -> Result<Markup, Box<dyn Error>> {
     let cv_file = read_to_string(Path::new("src/cv.json"))?;
-    let cv_items: Vec<CVItem> = serde_json::from_str(cv_file.as_str())?;
+    let cv_items:CV = serde_json::from_str(cv_file.as_str())?;
 
     let cv_html = html! {
-        h1{"job history"}
-        @for cv_item in cv_items {
-            h2{(cv_item.company ) " - " (cv_item.time)}
-            p{(cv_item.title)}
-            h3{"duties"}
+        div.row{
+            h1{"tom pridham"}
+            h2{"contact"}
             ul{
-                @for duty in cv_item.duties{
-                    li{(duty)}
+                li{"email: pridham.tom@gmail.com"}
+                li{"phone: 702-285-7906"}
+                li{"website: tompridham.github.io"}
+                li{"github: github.com/tompridham"}
+            }
+        }
+        div.row{
+            h2{"skills"}
+            ul{
+                li{"react"}
+                li{"graphql"}
+                li{"rust"}
+                li{"typescript"}
+                li{"angularjs"}
+                li{"node"}
+                li{"postgres"}
+            }
+        }
+        div.row{
+            h2{"job history"}
+            div{
+                @for cv_item in cv_items.work {
+                    h3{(cv_item.company ) " - " (cv_item.time)}
+                    p{(cv_item.title)}
+                    h4{"duties"}
+                    ul{
+                        @for duty in cv_item.duties{
+                            li{(duty)}
+                        }
+                    }
+                    h4{"notable achievements"}
+                    ul{
+                        @for n in cv_item.notable_achievements{
+                            li{(n)}
+                        }
+                    }
+                    h4{"technologies used"}
+                    ul{
+                        @for t in cv_item.technologies{
+                            li{(t)}
+                        }
+                    }
                 }
             }
-            h3{"notable achievements"}
-            ul{
-                @for n in cv_item.notable_achievements{
-                    li{(n)}
+        }
+        div.row{
+            h2{"projects"}
+            div{
+                @for project in cv_items.projects{
+                    h3{(project.title)}
+                    p{(project.description)}
+                    ul{
+                        @for tech in project.technologies{
+                            li{(tech)}
+                        }
+                    }
                 }
             }
-            h3{"technologies used"}
-            ul{
-                @for t in cv_item.technologies{
-                    li{(t)}
-                }
+        }
+        div.row{
+            h2{"education"}
+            div{
+                h3{"devmoutain - May 2016"}
+                p{"studied web development focused on the mean-stack. devmountain is a programming bootcamp - https://devmounta.in/web-immersive"}
+                h3{"salt lake community college - december 2015"}
+                p{"a.s. in mathematics"}
             }
         }
     };
