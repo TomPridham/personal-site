0a752a1e0bbbc9e0e726eb711fe22016535c3722
tompridham <pridham.tom@gmail.com>
2019-12-01 22:37:27 -0700

    tweaked some css

--- src/blog/svg_vs_icon_font.rs
+++ src/blog/svg_vs_icon_font.rs
@@ -14,8 +14,8 @@ pub fn svg_vs_icon_font() -> Result<Markup, Box<dyn std::error::Error>> {
             "two of the most common methods we saw were either using the svgs as the src in `img` tags or inlining the svg itself. using the svgs in `img` tags is an easy solution that really only requires the svgs be hosted somewhere to work most of the way. you end up having to do a lot of gross css workarounds that aren't supported everywhere and have differing implementations between browsers, however. "
         }
         pre{
-
-            code{
+            div.code{
+                code{
                 "
 const colorMap = {
   blue: 'filter: invert(39%) sepia(88%) saturate(1890%) hue-rotate(165deg) brightness(101%) contrast(101%)'
@@ -23,6 +23,7 @@ const colorMap = {
 const Icon = ({name, height, color}) =>
   <img src={`https://myHostingService.com/${name}`} style={{height, filter: colorMap[color]}}/>
 "
+                }
             }
         }
         p{
@@ -31,17 +32,19 @@ const Icon = ({name, height, color}) =>
         p{
             "the other method is inlining the svgs. initially this seems like it might be a good method, but it's shortcomings become apparent very quickly. inlining the svgs allows you to interact with them as HTML elements, which makes styling them a much nicer experience. but this method doesn't allow for caching by the browser which means that the icons will need to be fetched anew on every page load. it also results in a much heavier page because of the size of an svg is going to be larger byte-wise than an `img` tag with the src set. this only gets worse as you include more svg icons on your page. it also brings back the original problem of having to make code changes to add any new icons. it can also make bundle splitting out unused icons problematic if not done correctly. the below svg is about 5x larger than the equivalent `img` tag above and will need to be readded to the dom every time it is used"
         }
-        code{
+        div.code{
+            code{
             "
-                const Carrot = ({style}) => <svg style={style} xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><path d=\"M298.2 156.6c-52.7-25.7-114.5-10.5-150.2 32.8l55.2 55.2c6.3 6.3 6.3 16.4 0 22.6-3.1 3.1-7.2 4.7-11.3 4.7s-8.2-1.6-11.3-4.7L130.4 217 2.3 479.7c-2.9 6-3.1 13.3 0 19.7 5.4 11.1 18.9 15.7 30 10.3l133.6-65.2-49.2-49.2c-6.3-6.2-6.3-16.4 0-22.6 6.3-6.2 16.4-6.2 22.6 0l57 57 102-49.8c24-11.7 44.5-31.3 57.1-57.1 30.1-61.7 4.5-136.1-57.2-166.2zm92.1-34.9C409.8 81 399.7 32.9 360 0c-50.3 41.7-52.5 107.5-7.9 151.9l8 8c44.4 44.6 110.3 42.4 151.9-7.9-32.9-39.7-81-49.8-121.7-30.3z\"/></svg>
-                "
+const Carrot = ({style}) => <svg style={style} xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><path d=\"M298.2 156.6c-52.7-25.7-114.5-10.5-150.2 32.8l55.2 55.2c6.3 6.3 6.3 16.4 0 22.6-3.1 3.1-7.2 4.7-11.3 4.7s-8.2-1.6-11.3-4.7L130.4 217 2.3 479.7c-2.9 6-3.1 13.3 0 19.7 5.4 11.1 18.9 15.7 30 10.3l133.6-65.2-49.2-49.2c-6.3-6.2-6.3-16.4 0-22.6 6.3-6.2 16.4-6.2 22.6 0l57 57 102-49.8c24-11.7 44.5-31.3 57.1-57.1 30.1-61.7 4.5-136.1-57.2-166.2zm92.1-34.9C409.8 81 399.7 32.9 360 0c-50.3 41.7-52.5 107.5-7.9 151.9l8 8c44.4 44.6 110.3 42.4 151.9-7.9-32.9-39.7-81-49.8-121.7-30.3z\"/></svg>
+"
+            }
         }
         p{
             "we came up with a third method that is a little more complicated, but that brings all the benefits of both approaches with none of the downsides. you can fetch the svgs as images and then insert them into the DOM using `innerHtml` and then reference them with `use` tags. this allows for caching the responses, styling the svgs using normal svg selectors, only fetching required icons once."
         }
         pre{
-
-            code{
+            div.code{
+                code{
                 "
 const Icon = ({name}) => {
   if (!document.querySelector(`#${name}`)) {
@@ -61,6 +64,7 @@ const Icon = ({name}) => {
   return <svg><use href={`#${name}`} /></svg>
 }
                     "
+                }
             }
         }
     };
