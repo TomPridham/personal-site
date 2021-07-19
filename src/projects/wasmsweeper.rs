751a086b9a7fef0d7ce7c765b6eaab6d6f99d427
Tom Pridham <pridham.tom@gmail.com>
2021-07-19 11:15:56 -0600

    feat: add some instructions and disable the context menu

--- src/projects/wasmsweeper.rs
+++ src/projects/wasmsweeper.rs
@@ -8,17 +8,25 @@ pub fn wasmsweeper() -> Result<Markup, Box<dyn Error>> {
         p{
             "a minesweeper clone written in rust, using the library bevy, compiled into wasm. the repo is "
                 a href="https://github.com/tompridham/wasmsweeper"{"here"}
-            "."
+            ". it uses webgl2, so it doesn't currently work in safari. you might need to enable webgl2 for it to work. if it's not working in firefox, you might need to do: about:config => webgl.force-enabled = true"
         }
+        p{"click anywhere to get started. right click mines to flag them"}
         script type="module"{
 "
-  import init from './minesweeper.js';
-  init('./minesweeper_bg.wasm').then(function (wasm) {
+import init from './minesweeper.js';
+init('./minesweeper_bg.wasm').then(function (wasm) {
     wasm.run();
-  });
+});
 "
-
         }
+        script type="text/javascript"{
+"
+window.addEventListener('contextmenu', function(){
+    document.querySelector('canvas').oncontextmenu= function(e){e.preventDefault()}
+});
+"
+        }
+
     };
     Ok(wasmsweeper_html)
 }
