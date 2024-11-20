79aeef4bd4d0b069cb5095ebb504641665050905
Tom Pridham <pridham.tom@gmail.com>
2024-11-19 19:43:24 -0700

    feat: update some words

--- src/home.rs
+++ src/home.rs
@@ -6,13 +6,15 @@ pub fn home() -> Markup {
     html! {
         p{"Hi, I'm " (name) "!"}
         p{"Nice to meet you."}
-        p{"This is my website. It's written in Rust using a HTML templating library called Maud. I think it's pretty neat."}
+        p{"This is my website. It's written in Rust using a HTML templating library called Maud."}
+        p{"I like making things that are accessible, testable, and performant."}
         p{
             "My contact info is here."
             ul{
                 li{"Email: pridham.tom@gmail.com"}
                 li{"Phone: 702-285-7906"}
                 li{"Github: " a href="https://github.com/tompridham"{"github.com/tompridham"}}
+                li{"Linkedin: " a href="https://www.linkedin.com/in/tompridham/"{"linkedin.com/in/tompridham/"}}
             }
         }
     }
