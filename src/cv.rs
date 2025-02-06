f14e77c47e58bf4703d7a313a624a2f9cb1d14f4
Tom Pridham <pridham.tom@gmail.com>
2025-02-06 14:50:58 -0700

    feat: update cv to be more concise, fix some styling stuff

--- src/cv.rs
+++ src/cv.rs
@@ -9,6 +9,8 @@ use std::path::Path;
 struct Job {
     company: String,
     duties: Vec<String>,
+    #[serde(default)]
+    hide: bool,
     notable_achievements: Vec<String>,
     technologies: Vec<String>,
     time: String,
@@ -17,7 +19,11 @@ struct Job {
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Project {
+    demo_url: Option<String>,
     description: String,
+    #[serde(default)]
+    hide: bool,
+    repo_url: Option<String>,
     technologies: Vec<String>,
     title: String,
 }
@@ -34,10 +40,11 @@ pub fn cv() -> Result<Markup, Box<dyn Error>> {
             div.bottom-spacer{
                 h2.p-hide{"Contact"}
                 ul{
-                    li{"Email: pridham.tom@gmail.com"}
+                    li{"Email: " a href="mailto:pridham.tom@gmail.com"{"pridham.tom@gmail.com"}}
                     li{"Phone: 702-285-7906"}
-                    li{"Website: tompridham.me"}
-                    li{"Github: github.com/tompridham"}
+                    li{"Website: " a href="https://tompridham.me"{"tompridham.me"}}
+                    li{"Linkedin: " a href="https://www.linkedin.com/in/tompridham/"{"linkedin.com/in/tompridham/"}}
+                    li{"Github: " a href="https://github.com/tompridham"{"github.com/tompridham"}}
                 }
             }
         }
@@ -48,36 +55,38 @@ pub fn cv() -> Result<Markup, Box<dyn Error>> {
                 li{"Rust"}
                 li{"Node"}
                 li{"Typescript"}
-                li{"GraphQL"}
-                li{"AngularJS"}
-                li{"Postgres"}
+                li{"Performance"}
+                li{"Accessibility"}
+                li{"Webassembly"}
             }
         }
         div.bottom-spacer{
             h2{"Job History"}
             div{
                 @for cv_item in cv_items {
-                    div.bottom-spacer.hr.job{
-                        h3{(cv_item.company ) " | " (cv_item.time) " | " (cv_item.title)}
-                        h4{"Duties"}
-                        ul{
-                            @for duty in cv_item.duties{
-                                li{(duty)}
+                    @if !cv_item.hide{
+                        div.bottom-spacer.hr.job{
+                            h3{(cv_item.company ) " | " (cv_item.time) " | " (cv_item.title)}
+                            h4{"Duties"}
+                            ul{
+                                @for duty in cv_item.duties{
+                                    li{(duty)}
+                                }
                             }
-                        }
-                        h4{"Notable Achievements"}
-                        ul{
-                            @for n in cv_item.notable_achievements{
-                                li{(n)}
+                            h4{"Notable Achievements"}
+                            ul{
+                                @for n in cv_item.notable_achievements{
+                                    li{(n)}
+                                }
                             }
-                        }
-                        h4{"Technologies Used"}
-                        ul.p-twocol{
-                            @for t in cv_item.technologies{
-                                li{(t)}
+                            h4{"Technologies Used"}
+                            ul.p-twocol{
+                                @for t in cv_item.technologies{
+                                    li{(t)}
+                                }
                             }
-                        }
 
+                        }
                     }
                 }
             }
@@ -86,12 +95,22 @@ pub fn cv() -> Result<Markup, Box<dyn Error>> {
             h2 #projects{"Projects"}
             div{
                 @for project in projects{
-                    div.bottom-spacer.hr.project{
-                        h3{(project.title)}
-                        p{(project.description)}
-                        ul{
-                            @for tech in project.technologies{
-                                li{(tech)}
+                    @if !project.hide{
+                        div.bottom-spacer.hr.project{
+                            h3{(project.title)}
+                            @if project.demo_url.is_some() {
+                                @let demo_url = project.demo_url.unwrap();
+                                p{"Demo: " a href=(demo_url){(demo_url)}}
+                            }
+                            @if project.repo_url.is_some() {
+                                @let repo_url = project.repo_url.unwrap();
+                                p{"Repo: " a href=(repo_url){(repo_url)}}
+                            }
+                            p{(project.description)}
+                            ul.p-twocol{
+                                @for tech in project.technologies{
+                                    li{(tech)}
+                                }
                             }
                         }
                     }
@@ -100,7 +119,7 @@ pub fn cv() -> Result<Markup, Box<dyn Error>> {
         }
         div{
             h2{"Education"}
-            div{
+            div.education{
                 h3{"DevMoutain - May 2016"}
                 p{"Studied web development focused on the MEAN-Stack. DevMountain is a programming bootcamp - https://devmounta.in/web-immersive"}
                 h3{"Salt Lake Community College - December 2015"}
